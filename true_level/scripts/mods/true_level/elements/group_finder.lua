bf067fb59d5663afc164e8db3bcadf588b26b603
Baelwulf <114359227+alast-bot@users.noreply.github.com>
2026-06-30 22:22:25 +0100

    Update to fix display of havoc ranks post-Skitarii DLC
    Refactor group finder logic to handle player info and havoc rank. Improve safety checks for widget and member existence.

--- true_level/scripts/mods/true_level/elements/group_finder.lua
+++ true_level/scripts/mods/true_level/elements/group_finder.lua
@@ -2,18 +2,17 @@ local mod = get_mod("true_level")
 local ProfileUtils = require("scripts/utilities/profile_utils")
 local ref = "group_finder"
 
--- initialize
-
 mod:hook_safe(CLASS.GroupFinderView, "init", function(self)
     mod.desynced(ref)
 end)
 
 local _set_modified = function(widget, is_modified)
-    widget.tl_modified = is_modified
-    -- widget.wru_modified = is_modified
-end
+    if not widget then
+        return
+    end
 
--- desync
+    widget.tl_modified = is_modified
+end
 
 mod:hook_safe(CLASS.GroupFinderView, "_update_listed_group", function(self)
     mod.debug.echo("desync: listed group")
@@ -21,12 +20,12 @@ mod:hook_safe(CLASS.GroupFinderView, "_update_listed_group", function(self)
 
     local widgets = self._widgets_by_name
 
-    for i = 1, 4 do
-        local widget = widgets["team_member_" .. i]
+    if not widgets then
+        return
+    end
 
-        if widget then
-            _set_modified(widget, false)
-        end
+    for i = 1, 4 do
+        _set_modified(widgets["team_member_" .. i], false)
     end
 end)
 
@@ -54,17 +53,27 @@ mod:hook_safe(CLASS.GroupFinderView, "_populate_preview_grid", function(self)
     end
 end)
 
--- update
-
 local _get_player_info = function(account_id)
-    local social_service = Managers.data_service.social
+    local data_service = Managers and Managers.data_service
+    local social_service = data_service and data_service.social
 
     return account_id and social_service and social_service:get_player_info_by_account_id(account_id)
 end
 
-local _add_level = function(widget, profile)
+local _add_level = function(widget, profile, account_id, havoc_rank)
     local content = widget and widget.content
     local character_id = profile and profile.character_id
+
+    if not content or not character_id then
+        return
+    end
+
+    if account_id and havoc_rank then
+        mod.remember_havoc_rank(account_id, havoc_rank, character_id)
+    end
+
+    mod.ensure_profile_true_levels(profile, account_id, character_id)
+
     local true_levels = mod.get_true_levels(character_id)
 
     if true_levels then
@@ -77,8 +86,12 @@ end
 
 local _replace_listed_group = function(self)
     local own_group = self._own_group_visualization
-    local members = own_group.members
+    local members = own_group and own_group.members
     local widgets = self._widgets_by_name
+
+    if not members or not widgets then
+        return
+    end
 
     for i = 1, #members do
         local member = members[i]
@@ -88,7 +101,8 @@ local _replace_listed_group = function(self)
             local player_info = _get_player_info(member.account_id)
             local profile = player_info and player_info:profile()
 
-            _add_level(widget, profile)
+            mod.watch_havoc_player_info(player_info)
+            _add_level(widget, profile, member.account_id, member.havoc_rank_cadence_high)
         end
     end
 end
@@ -98,27 +112,24 @@ local _replace_grid = function(grid)
 
     for i = 1, #widgets do
         local widget = widgets[i]
-        local element = widget.content.element
+        local content = widget and widget.content
+        local element = content and content.element
         local presence_info = element and element.presence_info
 
         if presence_info then
             local profile = presence_info.profile
 
-            _add_level(widget, profile)
+            _add_level(widget, profile, element.account_id, presence_info.havoc_rank_cadence_high)
         end
     end
 end
 
 local _replace_preview_grid = function(self)
-    local grid = self._preview_grid
-
-    _replace_grid(grid)
+    _replace_grid(self._preview_grid)
 end
 
 local _replace_request_grid = function(self)
-    local grid = self._player_request_grid
-
-    _replace_grid(grid)
+    _replace_grid(self._player_request_grid)
 end
 
 mod:hook_safe(CLASS.GroupFinderView, "update", function(self)
@@ -128,11 +139,11 @@ mod:hook_safe(CLASS.GroupFinderView, "update", function(self)
 
     local state = self._state
 
-    if state == "browsing" then -- preview
+    if state == "browsing" then
         _replace_preview_grid(self)
         mod.synced(ref)
         mod.debug.echo("synced: browsing")
-    elseif state == "advertising" then -- own group / requests
+    elseif state == "advertising" then
         _replace_listed_group(self)
         _replace_request_grid(self)
         mod.synced(ref)
